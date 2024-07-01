262fbdf1798d992a5cfd38ad77de37cdeccf9b4f
Aleksandr Bogdanov <sasha_bogdanov_dev@yahoo.com>
2024-07-02 01:22:11 +0500

    SNAPSHOT-1

--- native/project.clj
+++ native/project.clj
@@ -1,4 +1,4 @@
-(def version "0.9.7-SNAPSHOT")
+(def version "0.9.7-SNAPSHOT-1")
 
 (defproject org.clojars.huahaiy/datalevin-native version
   :description "Datalevin GraalVM native image and command line tool"
