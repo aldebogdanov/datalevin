ee443348768d81d0a6f7487fc998ea789c5a1a27
Aleksandr Bogdanov <sasha_bogdanov_dev@yahoo.com>
2024-07-01 19:10:13 +0500

    package

--- native/src/java/datalevin/ni/ResourceLister.java
+++ native/src/java/datalevin/ni/ResourceLister.java
@@ -1,3 +1,5 @@
+package datalevin.ni;
+
 import java.io.IOException;
 import java.net.URI;
 import java.net.URL;
--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(def version "0.9.7-test-57")
+(def version "0.9.7-test-58")
 
 (defproject datalevin version
   :description "A simple, fast and versatile Datalog database"
